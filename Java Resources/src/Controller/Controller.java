d897327683f7db73b232f96f297ab188834d4371
david <david.abramovich84@gmail.com>
2019-09-03 22:59:24 -0300

    cambios

---
+++ Java Resources/src/Controller/Controller.java
@@ -0,0 +1,37 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import datos.GestorBD;
+import negocio.Productos;
+
+@WebServlet(name = "Controller", urlPatterns = {"/Controller"})
+public class Controller extends HttpServlet{
+
+	protected void processRequest(HttpServletRequest request,
+			HttpServletResponse response)throws ServletException, IOException{
+		
+		response.setContentType("text/html; charset = UTF-8");
+		
+		try {
+			
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			
+		}
+		catch(Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)throws ServletException, IOException{
+		
+		processRequest(request, response);
+	}
+
+}
