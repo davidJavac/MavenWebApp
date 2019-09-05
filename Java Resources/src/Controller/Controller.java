ca7ba63a003f45037caa560f567aca987bb03e71
david <david.abramovich84@gmail.com>
2019-09-04 22:55:19 -0300

    actualizacion pom

--- Java Resources/src/Controller/Controller.java
+++ Java Resources/src/Controller/Controller.java
@@ -3,11 +3,10 @@ package Controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import datos.GestorBD;
-import negocio.Productos;
 
 @WebServlet(name = "Controller", urlPatterns = {"/Controller"})
 public class Controller extends HttpServlet{
